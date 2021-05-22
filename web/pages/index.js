cb26626f92f66b31572c446e2f839e7da5abed37
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-05-22 18:35:14 +0530

    update meta

--- web/pages/index.js
+++ web/pages/index.js
@@ -23,14 +23,14 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="skillShack(⚡);" />
         <meta property="og:description" content="The Next-Generation Social Network for Software Professionals." />
-        <meta property="og:image" content="https://www.skillshack.dev/logoClipped.png" />
+        <meta property="og:image" content="https://www.skillshack.dev/letterLogoClippedColoredSocial.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="skillshack.dev" />
         <meta property="twitter:url" content="https://skillshack.dev" />
         <meta name="twitter:title" content="skillShack(⚡);" />
         <meta name="twitter:description" content="The Next-Generation Social Network for Software Professionals." />
-        <meta name="twitter:image" content="https://www.skillshack.dev/logoClipped.png" />
+        <meta name="twitter:image" content="https://www.skillshack.dev/letterLogoClippedColoredSocial.png" />
       </Head>
       <div className={styles.app}>
         {/* <NavBar /> */}
