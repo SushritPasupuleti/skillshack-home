b153d2a828cac3e163e14cf6b53f961640d1eff8
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-05-22 10:21:58 +0530

    Update index.js

--- web/pages/index.js
+++ web/pages/index.js
@@ -2,21 +2,25 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Typography from '@material-ui/core/Typography'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>skillShack</title>
-        <link rel="icon" href="/applogox64.ico" />
-      </Head>
-      <Typography variant="h2" color="initial">skillShack(⚡);</Typography>
-      <Typography variant="body2" color="initial">The Next-Generation Social Network for Software Professionals.</Typography>
-      <Image 
-      src="/undraw_career_progress_ivdb.svg"
-        width={500}
-        height={500}></Image>
-      <Typography variant="body2" color="initial">Coming Soon...</Typography>
-    </div>
+    <>
+      <NavBar />
+      <div className={styles.container}>
+        <Head>
+          <title>skillShack</title>
+          <link rel="icon" href="/applogox64.ico" />
+        </Head>
+        <Typography variant="h2" color="initial">skillShack(⚡);</Typography>
+        <Typography variant="body2" color="initial">The Next-Generation Social Network for Software Professionals.</Typography>
+        <Image
+          src="/undraw_career_progress_ivdb.svg"
+          width={500}
+          height={500}></Image>
+        <Typography variant="body2" color="initial">Coming Soon...</Typography>
+      </div>
+    </>
   )
 }
