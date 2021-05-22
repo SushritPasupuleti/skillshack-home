dafc3c7c8fe6fdaba51f5503d29d811d523ccb42
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-05-22 17:57:38 +0530

    Update index.js

--- web/pages/index.js
+++ web/pages/index.js
@@ -12,94 +12,101 @@ import {
 
 export default function Home() {
   return (
-    <div className={styles.app}>
-      {/* <NavBar /> */}
-      <div className={styles.appBody}>
-        <Relative pb={5} color="white">
-          <Absolute zIndex={1} left={0} right={0} top={0}>
-            <Flex is="header" p={3}>
-              <NavLink href="/" fontSize={3}>skillShack(⚡);</NavLink>
-              {/* <NavLink href="#" ml='auto'>Link 1</NavLink>
+    <>
+      <Head>
+        <title>skillShack</title>
+        <link rel="icon" href="/applogox64.ico" />
+        <meta property="og:image" content="/letterLogoClippedColored.png"/>
+      </Head>
+      <div className={styles.app}>
+        {/* <NavBar /> */}
+        <div className={styles.appBody}>
+          <Relative pb={5} color="white">
+            <Absolute zIndex={1} left={0} right={0} top={0}>
+              <Flex is="header" p={3}>
+                <NavLink href="/" fontSize={3}>skillShack(⚡);</NavLink>
+                {/* <NavLink href="#" ml='auto'>Link 1</NavLink>
               <NavLink href="#">Link 2</NavLink> */}
-            </Flex>
-          </Absolute>
-        </Relative>
-        <Hero
-          color="white"
-          bg="black"
-        // backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
-        >
-          <Heading>skillShack(⚡);</Heading>
-          <Subhead style={{ textAlign: 'center' }}>The Next-Generation Social Network for Software Professionals.</Subhead>
-          {/* <Image
+              </Flex>
+            </Absolute>
+          </Relative>
+          <Hero
+            color="white"
+            bg="black"
+          // backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
+          >
+            <Heading>skillShack(⚡);</Heading>
+            <Subhead style={{ textAlign: 'center' }}>The Next-Generation Social Network for Software Professionals.</Subhead>
+            {/* <Image
             src="/undraw_career_progress_ivdb.svg"
             width={500}
             height={500}></Image> */}
-          <CallToAction
-            // href="/getting-started"
-            mt={3}>Coming Soon</CallToAction>
-          <ScrollDownIndicator />
-        </Hero>
-        <Heading color="white" textAlign="center">What's inside?</Heading>
-        <Flex color="white" flexWrap="wrap" justifyContent="center">
-          <Feature icon="💡" description="Share Ideas">
-            Blogs, Videos, Podcasts
+            <CallToAction
+              // href="/getting-started"
+              mt={3}>Coming Soon</CallToAction>
+            <ScrollDownIndicator />
+          </Hero>
+          <Heading color="white" textAlign="center">What's inside?</Heading>
+          <Flex color="white" flexWrap="wrap" justifyContent="center">
+            <Feature icon="💡" description="Share Ideas">
+              Blogs, Videos, Podcasts
           </Feature>
-          <Feature icon="🔥" description="Stay upto date with the newest technologies and trends">
-            Curated Feed
+            <Feature icon="🔥" description="Stay upto date with the newest technologies and trends">
+              Curated Feed
           </Feature>
-          <Feature icon="🤔" description="Ask doubts, answers doubts, get noticed, brag!">
-            Forums
+            <Feature icon="🤔" description="Ask doubts, answers doubts, get noticed, brag!">
+              Forums
           </Feature>
-          <Feature icon="💼" description="Find jobs, create jobs, get the job done!">
-            Jobs/Internships
+            <Feature icon="💼" description="Find jobs, create jobs, get the job done!">
+              Jobs/Internships
           </Feature>
-          <Feature icon="🔮" description="And many more...">
-            ???
+            <Feature icon="🔮" description="And many more...">
+              ???
           </Feature>
-        </Flex>
-        <Section
-          bg='black'
-          color="white"
-          heading="What no Social Network has done before for Professionals"
-          subhead="Gamify learning!">
-          {/* Learn while you scroll through feeds endlessly. */}
-          <Checklist children={[
-            'Create',
-            'Share',
-            'Discuss',
-            'Grow',
-          ]} />
-          <CallToAction
-          // href='/getting-started'
-          >Get Notified!</CallToAction>
-        </Section>
-        <Section
-        style={{marginTop: '-6rem'}}
-          bg='black'
-          color="white">
-          <Heading textAlign="center">Made by</Heading>
-          <br/>
-          <Flex justifyContent="space-around">
-            <Contributor
-              fullName="Sushrit Pasupuleti"
-              title="CEO & CTO @ Braggi Solutions"
-              avatar="/me.jpg"
-            >
-              <Flex>
-                <NavLink href='https://sushritpasupuleti.github.io/'>View Profile</NavLink>
-              </Flex>
-            </Contributor>
           </Flex>
-        </Section>
-        <Flex color="white" is="footer" alignItems="center" p={3}>
-          <NavLink children="Instagram" href="#" />
-          <NavLink children="Twitter" href="#" />
-          <NavLink children="YouTube" href="#" />
-          <Small ml="auto">© skillShack(⚡); 2021</Small>
-        </Flex>
+          <Section
+            bg='black'
+            color="white"
+            heading="What no Social Network has done before for Professionals"
+            subhead="Gamify learning!">
+            {/* Learn while you scroll through feeds endlessly. */}
+            <Checklist children={[
+              'Create',
+              'Share',
+              'Discuss',
+              'Grow',
+            ]} />
+            <CallToAction
+            // href='/getting-started'
+            >Get Notified!</CallToAction>
+          </Section>
+          <Section
+            style={{ marginTop: '-6rem' }}
+            bg='black'
+            color="white">
+            <Heading textAlign="center">Made by</Heading>
+            <br />
+            <Flex justifyContent="space-around">
+              <Contributor
+                fullName="Sushrit Pasupuleti"
+                title="CEO & CTO @ Braggi Solutions"
+                avatar="/me.jpg"
+              >
+                <Flex>
+                  <NavLink href='https://sushritpasupuleti.github.io/'>View Profile</NavLink>
+                </Flex>
+              </Contributor>
+            </Flex>
+          </Section>
+          <Flex color="white" is="footer" alignItems="center" p={3}>
+            <NavLink children="Instagram" href="#" />
+            <NavLink children="Twitter" href="#" />
+            <NavLink children="YouTube" href="#" />
+            <Small ml="auto">© skillShack(⚡); 2021</Small>
+          </Flex>
+        </div>
+        {/* <Footer /> */}
       </div>
-      {/* <Footer /> */}
-    </div>
+    </>
   )
 }
