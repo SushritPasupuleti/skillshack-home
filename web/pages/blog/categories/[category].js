8f85dc1a0d659d4e97b08e476336b1d88348a49a
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-11 20:26:54 +0530

    Update [category].js

--- web/pages/blog/categories/[category].js
+++ web/pages/blog/categories/[category].js
@@ -39,6 +39,7 @@ export async function getStaticProps(context) {
                     id
                     title
                     content
+                    excerpt
                   }
                 }
               }
