9bc027de756ccba05d519f2a1e2b612990fbc3bf
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-11 19:58:36 +0530

    Create Categories.js

---
+++ web/components/Categories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Chip from '@material-ui/core/Chip';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: '1rem',
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(0.5),
+        },
+        // paddingLeft: '1rem'
+    },
+    chip: {
+        // marginRight: '0.5rem',
+    }
+}));
+
+const ThreadTags = (props) => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            {props.thread.tags.map((tag, i) => {
+                return (
+                    <Chip color="primary" label={tag.name} key={tag.name} className={classes.chip} />
+                )
+            })}
+        </div>
+    )
+}
+
+export default ThreadTags
