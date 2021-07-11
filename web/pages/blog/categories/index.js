c842be75138417134a2efc394f888557d2172518
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-11 21:09:57 +0530

    Update index.js

--- web/pages/blog/categories/index.js
+++ web/pages/blog/categories/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-function Home() {
+export default function Home() {
   return (
     <ul>
       <li>
@@ -22,4 +22,38 @@ function Home() {
   )
 }
 
-export default Home
+export async function getStaticProps(context) {
+
+  const res = await fetch('https://dmrks.org/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+          query: `
+          query AllCategoriesQuery {
+            categories {
+                nodes {
+                  id
+                  slug
+                  name
+                }
+              }
+        }
+          `,
+      })
+  })
+
+  console.log("check1")
+
+  const json = await res.json()
+
+  console.log("check2")
+
+  console.log("Returned: ", json.data.categories.nodes)
+
+  return {
+      props: {
+          categories: json.data.categories,
+      },
+  }
+
+}
