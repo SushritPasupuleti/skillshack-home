d88158ed78d9d7adbe6a4d5c77d290cddfaf6095
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-11 20:06:25 +0530

    add categories index page

---
+++ web/pages/blog/categories/index.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+function Home() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About Us</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/blog/hello-world">
+          <a>Blog Post</a>
+        </Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Home
