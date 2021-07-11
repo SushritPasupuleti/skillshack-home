703a6c557583e497eb79e1bad8c16fc61b40ffd4
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-11 20:25:22 +0530

    add queries

---
+++ web/pages/blog/categories/[category].js
@@ -0,0 +1,96 @@
+import Link from 'next/link'
+
+export default function Home(data) {
+
+    // console.log("Context: ", context.params)
+    console.log("Posts: ", data.posts)
+
+    return (
+        <ul>
+            <li>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/about">
+                    <a>About Us</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/blog/hello-world">
+                    <a>Blog Post</a>
+                </Link>
+            </li>
+        </ul>
+    )
+}
+
+export async function getStaticProps(context) {
+
+    const res = await fetch('https://dmrks.org/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            query: `
+            query GetCategoryPosts($categoryName: String) {
+                posts(where: {categoryName: $categoryName}) {
+                  nodes {
+                    id
+                    title
+                    content
+                  }
+                }
+              }
+            `,
+            variables: {
+                categoryName: context.params.category,
+            }
+        })
+    })
+
+    console.log("check1")
+
+    const json = await res.json()
+
+    console.log("check2")
+
+    console.log("Returned: ", json.data.posts)
+
+    return {
+        props: {
+            posts: json.data.posts,
+        },
+    }
+
+}
+
+export async function getStaticPaths() {
+
+    const res = await fetch('https://dmrks.org/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            query: `
+            query AllCategoriesQuery {
+                categories {
+                    nodes {
+                      id
+                      slug
+                      name
+                    }
+                  }
+            }
+        `})
+    })
+
+    const json = await res.json()
+    const categories = json.data.categories.nodes;
+
+    const paths = categories.map((category) => ({
+        params: { category: category.slug },
+    }))
+
+    return { paths, fallback: false }
+
+}
