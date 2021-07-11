b02283c76efa9a3888e5feccd22c17ce9167dff0
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-11 19:56:11 +0530

    make iframes responsive

--- web/components/CustomMarkdownRender.js
+++ web/components/CustomMarkdownRender.js
@@ -3,40 +3,46 @@ import ReactMarkdown from 'markdown-to-jsx';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import stylesStatic from './Markdown.module.css';
 
 const styles = (theme) => ({
-  listItem: {
-    marginTop: theme.spacing(1),
-  },
+    listItem: {
+        marginTop: theme.spacing(1),
+    },
 });
 
 const options = {
-  overrides: {
-    h1: {
-      component: Typography,
-      props: {
-        gutterBottom: true,
-        variant: 'h5',
-      },
+    overrides: {
+        h1: {
+            component: Typography,
+            props: {
+                gutterBottom: true,
+                variant: 'h5',
+            },
+        },
+        h2: { component: Typography, props: { gutterBottom: true, variant: 'h2' } },
+        h3: { component: Typography, props: { gutterBottom: true, variant: 'h3' } },
+        h4: {
+            component: Typography,
+            props: { gutterBottom: true, variant: 'h4', paragraph: true },
+        },
+        p: { component: Typography, props: { paragraph: true, variant: 'body1' } },
+        a: { component: Link },
+        li: {
+            component: withStyles(styles)(({ classes, ...props }) => (
+                <li className={classes.listItem}>
+                    <Typography component="span" {...props} />
+                </li>
+            )),
+        },
     },
-    h2: { component: Typography, props: { gutterBottom: true, variant: 'h2' } },
-    h3: { component: Typography, props: { gutterBottom: true, variant: 'h3' } },
-    h4: {
-      component: Typography,
-      props: { gutterBottom: true, variant: 'h4', paragraph: true },
-    },
-    p: { component: Typography, props: { paragraph: true, variant: 'body1' } },
-    a: { component: Link },
-    li: {
-      component: withStyles(styles)(({ classes, ...props }) => (
-        <li className={classes.listItem}>
-          <Typography component="span" {...props} />
-        </li>
-      )),
-    },
-  },
 };
 
 export default function Markdown(props) {
-  return <ReactMarkdown options={options} {...props} />;
+    return (
+        <div className={stylesStatic.markdownParent}>
+            <div className={stylesStatic.markdownDiv}>
+                <ReactMarkdown options={options} {...props} />
+            </div>
+        </div >);
 }
