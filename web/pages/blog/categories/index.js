ee8b817cdfefd1a939d2d0bd10d7377c3611af33
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-11 23:48:04 +0530

    Update index.js

--- web/pages/blog/categories/index.js
+++ web/pages/blog/categories/index.js
@@ -2,11 +2,30 @@ import Link from 'next/link'
 import Categories from '../../../components/Categories';
 import Typography from '@material-ui/core/Typography';
 import NavBar from '../../../components/NavBar';
+import Head from 'next/head'
 
 export default function CategoriesPage(data) {
   console.log("Categories: ", data.categories);
   return (
-    <div style={{marginTop: '1rem', padding: '5%'}}>
+    <div style={{ marginTop: '1rem', padding: '5%' }}>
+      <Head>
+        <title>skillShack(⚡).Blog;</title>
+        <link rel="icon" href="/applogox64.ico" />
+        <meta name="description" content="The Next-Generation Social Network for Software Professionals." />
+
+        <meta property="og:url" content="https://skillshack.dev" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="skillShack(⚡);" />
+        <meta property="og:description" content="The Next-Generation Social Network for Software Professionals." />
+        <meta property="og:image" content="https://www.skillshack.dev/letterLogoClippedColoredSocial.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="skillshack.dev" />
+        <meta property="twitter:url" content="https://skillshack.dev" />
+        <meta name="twitter:title" content="skillShack(⚡);" />
+        <meta name="twitter:description" content="The Next-Generation Social Network for Software Professionals." />
+        <meta name="twitter:image" content="https://www.skillshack.dev/letterLogoClippedColoredSocial.png" />
+      </Head>
       <NavBar></NavBar>
       <Typography variant="h1" component="h2" gutterBottom>
         Categories
