860e19191923f8c26fc284b84d91b07c5c1d52fb
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-11 17:58:45 +0530

    Update Container.js

--- web/components/Container.js
+++ web/components/Container.js
@@ -1,4 +1,4 @@
 export default function Container({ children }) {
-    <Container maxWidth="sm" style={{padding: '5px'}}>{children}</Container>
+    return <Container maxWidth="sm" style={{padding: '5px'}}>{children}</Container>
   }
   
