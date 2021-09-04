38dc615a0bb20a8803d4f74b46d0b65d555e7c58
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-09-04 11:10:56 +0530

    Create Header.js

---
+++ web/components/sections/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export default function Header() {
+    return (
+        <div>
+            <Typography variant="h1" color="initial">skillShack(⚡);</Typography>
+        </div>
+    )
+}
