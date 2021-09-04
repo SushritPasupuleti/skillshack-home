d2857a864ec3b16ceaa75eedf34283d918248ec2
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-09-04 11:15:42 +0530

    Update home.js

--- web/pages/home.js
+++ web/pages/home.js
@@ -51,6 +51,8 @@ export default function Home() {
                         <br />
                         <CallToAction
                             mt={3}><a href='https://forms.gle/pxkC7VqkKqFVkS1s6' target="_blank">Coming Soon! Get Nofified</a></CallToAction>
+                        <br />
+                        <img style={{height: '50vh'}} src="https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"></img>
                         <ScrollDownIndicator />
                     </Hero>
                 </div>
